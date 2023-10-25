1de684d85a7e97ea9dc3c7a39ab1c8f33dcf60a3
nguyenchiemdu <nnguenchiemdu@gmail.com>
2023-10-25 23:24:14 +0700

    feature: base code

---
+++ prisma/index.ts
@@ -0,0 +1,26 @@
+import { PrismaClient } from '@prisma/client';
+
+const prisma = new PrismaClient();
+
+async function main() {
+    await prisma.user.create({
+        data: {
+            username: 'Alice',
+            password: '123456',
+        },
+    })
+
+    const allUsers = await prisma.user.findMany({
+    })
+    console.dir(allUsers, { depth: null })
+}
+
+main()
+    .then(async () => {
+        await prisma.$disconnect();
+    })
+    .catch(async (e) => {
+        console.error(e);
+        await prisma.$disconnect();
+        process.exit(1);
+    });
